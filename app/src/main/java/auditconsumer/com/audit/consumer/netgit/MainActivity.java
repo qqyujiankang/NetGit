aaa2915e975d5363a0f54f84c2f420958c14704e
YJK <18518241515@163.coM>
2018-10-23 15:52:52 +0800

    165464646

--- app/src/main/java/auditconsumer/com/audit/consumer/netgit/MainActivity.java
+++ app/src/main/java/auditconsumer/com/audit/consumer/netgit/MainActivity.java
@@ -3,6 +3,9 @@ package auditconsumer.com.audit.consumer.netgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * kjgju
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
